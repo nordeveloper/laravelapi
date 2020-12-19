cfe9b473823f06de51dbc0b8dc23e572f3fa255a
nordeveloper <nordeveloper@gmail.com>
2020-12-19 21:00:55 +0400

    updated

--- app/Http/Controllers/PostlikeController.php
+++ app/Http/Controllers/PostlikeController.php
@@ -10,6 +10,22 @@ class PostlikeController extends Controller
     public function store(Request $request)
     {
 
-        return PostLike::create($request->all());
+        // dump($request['liks']);
+        // if(  OR !empty($request['disliks']) ){
+        //     return PostLike::create($request->all());
+        // }  
+        
+        $data = new PostLike();
+
+        if(!empty($request['like'])){
+            $data->liks=$request->like;
+        }
+        
+        if(!empty($request['dislik'])){
+            $data->disliks=$request->dislik;
+        }
+
+        $data->post_id = $request->post_id;
+        return $data->save();        
     }
 }
